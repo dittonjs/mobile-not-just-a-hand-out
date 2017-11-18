d69e3dd7c319e092e40098096e4dd184e538ac97
Austin Adams <austinadams@Austins-MacBook-Pro.local>
2017-11-18 07:59:59 -0700

    added side swipe to get sidebar for pin filters

--- App.js
+++ App.js
@@ -1,70 +1,22 @@
-import React from 'react';
-import { StyleSheet, Text, View, Dimensions, ScrollView } from 'react-native';
-import MapView, { PROVIDER_GOOGLE } from 'react-native-maps';
+import React, { Component } from 'react';
+import { AppRegistry,View,Text,StyleSheet,ScrollView,TouchableOpacity } from 'react-native';
+import { StackNavigator,DrawerNavigator  } from 'react-navigation';
 
-const { width, height } = Dimensions.get('window');
-const SCREEN_WIDTH = width;
-const ASPECT_RATIO = width / height;
-const LATITUDE = 37.78825;
-const LONGITUDE = -122.4324;
-const LATITUDE_DELTA = 0.0922;
-const LONGITUDE_DELTA = LATITUDE_DELTA * ASPECT_RATIO;
+import HomeScreen from './home';
+import Products from './products';
+import Filter from  './filter';
 
-export default class Analysis extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      region: {
-        latitude: LATITUDE,
-        longitude: LONGITUDE,
-        latitudeDelta: LATITUDE_DELTA,
-        longitudeDelta: LONGITUDE_DELTA,
-      },
-    };
-  }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <View style={styles.footer}>
-        </View>
-        <MapView
-          provider={PROVIDER_GOOGLE}
-          style={styles.map}
-          scrollEnabled={true}
-          zoomEnabled={true}
-          pitchEnabled={true}
-          rotateEnabled={true}
-          initialRegion={this.state.region}
-        >
-          <MapView.Marker
-            title="This is a title"
-            description="This is a description"
-            coordinate={this.state.region}
-          />
-        </MapView>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    ...StyleSheet.absoluteFillObject,
-    alignItems: 'center',
-  },
-  scrollview: {
-    alignItems: 'center',
-  },
-  map: {
-    width: SCREEN_WIDTH,
-    height: height - 75,
-  },
-  footer: {
-    height: 75,
-    width: SCREEN_WIDTH,
-    backgroundColor: 'grey'
-  }
+const myDrawer = DrawerNavigator({
+  Home: { screen: HomeScreen },
+  Products: { screen: Products }, 
+},
+{
+  contentComponent: props => <Filter {...props} />
 });
+
+const nativeShop = StackNavigator({
+  Home: { screen: myDrawer },
+  
+ });
+export default nativeShop;
 
---
+++ locations.js
@@ -0,0 +1,1 @@
+//use this file to get locations from db
